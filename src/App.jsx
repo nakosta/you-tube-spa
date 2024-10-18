f61f4b89d916d1cc71401dafc3c04efddafe4711
Konstantin Karpov <nakosta257514@mail.ru>
2024-10-18 07:02:08 +0300

    LifeCycle2

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
+import LifecycleComponent1 from "./components/LifecycleComponent1";
 import LifecycleComponent2 from "./components/LifecycleComponent2";
 
+
 const App = () => {
+
+  const [count, setCount] = useState(0)
+
   return (
     <div>
-      <LifecycleComponent2 />
+      {count}
+      <button onClick={() => setCount(count => count + 1)}>KILL</button>
+      {count % 2 === 0 ? <LifecycleComponent1 /> : <p>LOX</p>}
     </div>
   );
 };
