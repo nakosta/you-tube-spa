cdacf459701a4be561e85a0fa7b50967f6596231
Konstantin Karpov <nakosta257514@mail.ru>
2024-10-26 19:45:21 +0300

    viktorya

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from "react";
+import React, { useState, useCallback, useMemo } from "react";
 import SearchInput from "./components/SearchInput";
 import ItemList from "./components/ItemList";
 import CounterButton from "./components/CounterButton";
@@ -6,12 +6,15 @@ import CounterButton from "./components/CounterButton";
 const App = () => {
   console.log("App");
 
-  const arr = [
-    { name: "Kostya", id: 1 },
-    { name: "Tanya", id: 2 },
-    { name: "Pasha", id: 3 },
-    { name: "Masha", id: 4 },
-  ];
+  const arr = useMemo(
+    () => [
+      { name: "Kostya", id: 1 },
+      { name: "Tanya", id: 2 },
+      { name: "Pasha", id: 3 },
+      { name: "Masha", id: 4 },
+    ],
+    []
+  );
 
   const [count, setCount] = useState(0);
 
@@ -28,7 +31,10 @@ const App = () => {
   return (
     <>
       <CounterButton count={count} incrementCount={incrementCount} />
-      <SearchInput inputValue={inputValue} updateInputValue={updateInputValue} />
+      <SearchInput
+        inputValue={inputValue}
+        updateInputValue={updateInputValue}
+      />
       <ItemList inputValue={inputValue} arr={arr} />
     </>
   );
