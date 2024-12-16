7ba61258fca4d1cf789a907fbc2d11f05584119f
Konstantin Karpov <nakosta257514@mail.ru>
2024-12-16 12:56:29 +0300

    finish3

--- src/api.jsx
+++ src/api.jsx
@@ -38,7 +38,7 @@ export async function login(emailPassword) {
   }
 }
 
-export async function fetchVideos({query, maxResults, order}) {
+export async function fetchVideos({ query, maxResults, order }) {
   try {
     const API_KEY = import.meta.env.VITE_YOUTUBE_API_KEY;
     const BASE_URL = import.meta.env.VITE_YOUTUBE_URL;
@@ -53,8 +53,6 @@ export async function fetchVideos({query, maxResults, order}) {
       },
     });
 
-    console.log(data);
-
     const videoIds = data.items.map((item) => item.id.videoId).join(",");
 
     const videoDetails = await axios.get(`${BASE_URL}/videos`, {
@@ -65,8 +63,6 @@ export async function fetchVideos({query, maxResults, order}) {
       },
     });
 
-    console.log(videoDetails.data.items);
-    
     return {
       videos: videoDetails.data.items.map((item) => ({
         id: item.id,
@@ -84,7 +80,6 @@ export async function fetchVideos({query, maxResults, order}) {
   }
 }
 
-
 // "id": 723,
 // "username": "nakosta666",
 // "email": "nakosta666@mail.ru",
@@ -94,8 +89,3 @@ export async function fetchVideos({query, maxResults, order}) {
 
 // "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im5ha29zdGE2NjZAbWFpbC5ydSIsImlkIjo3MjMsImlhdCI6MTcxNzEzMTA4MX0.kly-kubaJsGpD6-rfWob5SMZiBGUx9IjCdqVa5bi1Mc"
 
-// API-ключ из Google - AIzaSyBONMMGfs_icZgSJxpor51OoB6DElEwZ4w
-
-// 2 API-ключ из Google - AIzaSyAc4i5q8QZCHW1PODzyff05F03H_xS9UzQ
-
-// API-ключ из Google Кости - AIzaSyBRbw7E44FNOaUi4VGBizBk6MnmpS8F4Bo
